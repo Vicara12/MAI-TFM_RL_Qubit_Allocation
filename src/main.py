5cbc7277a278f0d7f69915fe8a083de4e1398892
Victor Carballo <v.carballo@kaliber.ai>
2025-11-24 04:53:04 -0800

    fix: Small parameter change in main

--- src/main.py
+++ src/main.py
@@ -100,14 +100,14 @@ def train_model_da(architecture: list[int], name: str):
     train_iters=1_000,
     batch_size=4,
     group_size=16,
-    validate_each=2, # !
+    validate_each=25,
     validation_hardware=validation_hardware,
     validation_circuits=[val_sampler.sample() for _ in range(64)],
     store_path=f"trained/{name}",
-    initial_noise=0.4,
-    noise_decrease_factor=0.995,
+    initial_noise=0.8,
+    noise_decrease_factor=0.99,
     circ_sampler=RandomCircuit(num_lq=32, num_slices=lambda: randint(8,32)),
-    lr=2.5e-5,
+    lr=5e-4,
     hardware_sampler=HardwareSampler(max_nqubits=32, range_ncores=[2,8]),
   )
   allocator.train(train_cfg)
